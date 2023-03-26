60e46f575ac95b9d45f2b8efaec7d41be3862c94
Abdulaziz1920 <abdulaziz090203@gmail.com>
2023-03-26 09:49:45 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+let modeBtn = document.getElementById("dark-light");
+
+modeBtn.addEventListener("click", function () {
+  if (document.body.className != "dark") {
+    this.firstElementChild.src = "icon/light.svg";
+  } else {
+    this.firstElementChild.src = "icon/dark.svg";
+  }
+  document.body.classList.toggle("dark");
+});
+
+window.addEventListener("scroll", function () {
+  shrink();
+});
+
+let navbar = document.getElementById("navbar");
+
+function shrink() {
+  if (scrollY > 100) {
+    navbar.classList.add("navbar-shrink");
+  } else {
+		navbar.classList.remove("navbar-shrink");
+	}
+}
